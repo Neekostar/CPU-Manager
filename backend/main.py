20c2f75e0e1c928a5cda52312350c5d3f070e4f3
neekostar <nekitcus@mail.ru>
2024-07-08 11:13:18 +0300

    Added main logic

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+import uvicorn
+import models
+import crud
+from database import SessionLocal, engine
+from utils import save_cpu_load
+import asyncio
+import threading
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+def run_save_cpu_load():
+    db = SessionLocal()
+    asyncio.run(save_cpu_load(db))
+
+
+@app.on_event("startup")
+def startup_event():
+    threading.Thread(target=run_save_cpu_load, daemon=True).start()
+
+
+@app.get("/cpu_load_last_hour")
+def get_cpu_load_last_hour(db: Session = Depends(get_db)):
+    cpu_loads = crud.get_cpu_loads_last_hour(db)
+    return [{"timestamp": load.timestamp, "value": load.value} for load in cpu_loads]
+
+
+@app.get("/average_load_last_hour")
+def get_average_load_last_hour(db: Session = Depends(get_db)):
+    avg_load = crud.compute_average_load_per_minute(db)
+    return {"average_load_last_hour": avg_load}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
